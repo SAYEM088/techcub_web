29aead1019a4f249f3b4b197c03b0b774f22ac55
unknown <abusayemsarker088@gmail.com>
2025-10-20 16:59:48 +0600

    first code for techcub

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <div className="text-cyan-500 text-xl font-bold text-center">Hi TechCub</div>
+   </div>
+  );
+}
